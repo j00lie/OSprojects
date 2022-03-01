2dba5228362c0e2ac3c6f3835248cec6f1779690
j00lie <77616344+j00lie@users.noreply.github.com>
2022-03-01 12:55:04 +0200

    -

--- p1/reverse.c
+++ p1/reverse.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define _POSIX_C_SOURCE 200809L
+
+void readFile()
+{
+    FILE *f;
+    char *buffer; // lines here
+    size_t bufsize = 32;
+    size_t lines;
+
+    buffer = (char *)malloc(bufsize * sizeof(char));
+    f = fopen("input.txt", "r");
+    while ((lines = getline(&buffer, &bufsize, f)) != -1)
+    {
+        printf("%s", buffer);
+    }
+    fclose(f);
+}
+
 int main(void)
 {
-    printf("hello world");
+    readFile();
     return 0;
 }
