722fda7050731be165ec87ce79f1001f54df4e2a
j00lie <77616344+j00lie@users.noreply.github.com>
2022-03-01 15:05:07 +0200

    -

--- p1/reverse.c
+++ p1/reverse.c
@@ -1,25 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define _POSIX_C_SOURCE 200809L
+#include <string.h>
 
-void readFile()
+typedef struct line
+{
+    struct line *pNext;
+    char line[];
+} LINE;
+
+// Create new node to represent a line
+LINE *newLine()
+{
+    LINE *new = (LINE *)malloc(sizeof(LINE));
+    new->pNext = NULL;
+    return new;
+}
+
+LINE *readFile()
 {
     FILE *f;
-    char *buffer; // lines here
+    char *buffer; // line here
     size_t bufsize = 32;
-    size_t lines;
+    size_t line;
+    LINE *pStart = NULL, *ptr = NULL, *pNew;
 
     buffer = (char *)malloc(bufsize * sizeof(char));
     f = fopen("input.txt", "r");
-    while ((lines = getline(&buffer, &bufsize, f)) != -1)
+    while (line = getline(&buffer, &bufsize, f) != -1)
     {
-        printf("%s", buffer);
+        if (pStart == NULL)
+        {
+            pStart = newLine();
+            strcpy(pStart->line, buffer);
+            // printf("rivi: %s", ptr->line);
+            ptr = pStart;
+        }
+        else
+        {
+            pNew = newLine();
+            ptr->pNext = pNew;
+            strcpy(pNew->line, buffer);
+            ptr = ptr->pNext;
+        }
     }
+
     fclose(f);
+    free(ptr);
+    return pStart;
+}
+void printList(LINE *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%s\n", ptr->line);
+        ptr = ptr->pNext;
+    }
 }
 
 int main(void)
 {
-    readFile();
+    LINE *ptr = NULL;
+    ptr = readFile(ptr);
+    printList(ptr);
+    free(ptr);
     return 0;
 }
